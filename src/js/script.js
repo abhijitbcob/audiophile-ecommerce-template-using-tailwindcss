a54ec2f3877e1505b3894ad32fd8cc18a9fa5a77
Abhijit Barman <49182619+abhijitbcob@users.noreply.github.com>
2021-11-26 10:21:05 +0530

    misc change

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
 const header = document.querySelector("header");
 const menuToggler = document.querySelector("#menu-toggler");
 const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
 
 menuToggler.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    // overlay.classList.add("z-[-1]");
   } else {
     header.classList.add("open");
+    // overlay.classList.remove("z-[-1]");
   }
 });
